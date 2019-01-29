ff92b1ea61e8d2d77d216f883f02f19264a956bb
Thijs de Jong <tde-jong@student.codam.nl>
2019-01-29 09:03:40 +0100

    Update fillit.c

--- fillit.c
+++ fillit.c
@@ -6,7 +6,7 @@
 /*   By: tde-jong <tde-jong@student.codam.nl>         +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2019/01/26 15:55:14 by tde-jong       #+#    #+#                */
-/*   Updated: 2019/01/29 09:01:55 by tde-jong      ########   odam.nl         */
+/*   Updated: 2019/01/29 09:03:37 by tde-jong      ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,4 +24,5 @@ int	main(int argc, char **argv)
 		ft_putstr(argv[0]);
 		ft_putstr(" source_file\n");
 	}
+	return (0);
 }
