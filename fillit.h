b06330b3e23dff5f2557434519a248a6887e5786
jandre-d <jandre-d@student.codam.nl>
2019-01-31 14:00:52 +0100

    .
    .

--- fillit.h
+++ fillit.h
@@ -6,7 +6,7 @@
 /*   By: jandre-d <jandre-d@student.codam.nl>         +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2019/01/26 15:55:39 by tde-jong       #+#    #+#                */
-/*   Updated: 2019/01/31 14:00:05 by jandre-d      ########   odam.nl         */
+/*   Updated: 2019/01/31 14:00:47 by jandre-d      ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,6 +20,7 @@ typedef struct			s_tetrimino
 	char				**array;
 	char				w;
 	char				h;
+	char				end_char;
 	struct s_tetrimino	*next;
 }						t_tetrimino;
 
