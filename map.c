9435c2ceb89f2c82623fefdab85d33ed71a22577
jandre-d <jandre-d@student.codam.nl>
2019-01-31 18:08:08 +0100

    .
    .

--- map.c
+++ map.c
@@ -6,22 +6,52 @@
 /*   By: jandre-d <jandre-d@student.codam.nl>         +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2019/01/31 14:34:09 by jandre-d       #+#    #+#                */
-/*   Updated: 2019/01/31 15:21:44 by jandre-d      ########   odam.nl         */
+/*   Updated: 2019/01/31 18:06:38 by jandre-d      ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "map.h"
 
-static t_byte	set_initial_size_sqrt(int block_count)
+static t_byte	get_initial_size_sqrt(int block_count)
 {
-	if (block_count == 0)
-		return (0);
+	int a;
 
+	a = 0;
+	while (1)
+	{
+		if ((a * a) >= block_count)
+			return (a);
+		a++;
+	}
+	return (0);
 }
 
-static t_byte	set_initial_size_count(t_tetrimino list)
+static int	get_input_block_count(t_tetrimino *list)
 {
+	int			block_count;
+	t_tetrimino *lst;
+	t_byte		i;
+	t_byte		j;
 
+	block_count = 0;
+	lst = list;
+	while (lst != NULL)
+	{
+		i = 0;
+		while (j < lst->h)
+		{
+			j = 0;
+			while (i < lst->w)
+			{
+				if (lst->array[i][j] == '#')
+					block_count++;
+				j++;
+			}
+			i++;
+		}
+		lst = lst->next;
+	}
+	return (block_count);
 }
 
 t_byte	set_initial_size(t_state state)
