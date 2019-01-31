10dcc5384c132ad2f028121d608847fb28269233
Thijs de Jong <tde-jong@student.codam.nl>
2019-01-31 18:31:33 +0100

    revert back to assert

--- test/input_check.c
+++ test/input_check.c
@@ -6,7 +6,7 @@
 /*   By: tde-jong <tde-jong@student.codam.nl>         +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2019/01/30 14:21:18 by tde-jong       #+#    #+#                */
-/*   Updated: 2019/01/31 18:27:45 by tde-jong      ########   odam.nl         */
+/*   Updated: 2019/01/31 18:31:24 by tde-jong      ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,21 +24,17 @@ int	main(int argc, char **argv)
 		system(ft_strjoin("cd input_gen && python tetriminos.py ", argv[1]));
 		if (ft_atoi(argv[1]) >= 1 && ft_atoi(argv[1]) <= 26)
 		{
-			if (validate_input("input_gen/ttmnfile") != 1)
-				abort();
-			if (parse_input("input_gen/ttmnfile", state) != 1)
-				abort();
+			assert(validate_input("input_gen/ttmnfile") == 1);
+			assert(parse_input("input_gen/ttmnfile", state) == 1);
 			ft_putendl("input is valid, this is correct");
 		}
 		else
 		{
-			if (validate_input("input_gen/ttmnfile") != 0)
-				abort();
-			if (parse_input("input_gen/ttmnfile", state) != 0)
-				abort();
+			assert(validate_input("input_gen/ttmnfile") == 0);
+			assert(parse_input("input_gen/ttmnfile", state) == 0);
 			ft_putendl("input is invalid, this is correct");
 		}
 		return (0);
 	}
-	abort();
+	return (1);
 }
