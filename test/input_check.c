92cd226d26980d96376efe08851010a5c1adb716
Thijs de Jong <tde-jong@student.codam.nl>
2019-01-31 18:11:58 +0100

    first attempt to add test, might break

--- test/input_check.c
+++ test/input_check.c
@@ -6,27 +6,32 @@
 /*   By: tde-jong <tde-jong@student.codam.nl>         +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2019/01/30 14:21:18 by tde-jong       #+#    #+#                */
-/*   Updated: 2019/01/30 14:40:07 by tde-jong      ########   odam.nl         */
+/*   Updated: 2019/01/31 18:11:46 by tde-jong      ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../libft/libft.h"
 #include "../validate_input.c"
+#include "../parse_input.c"
+#include "../fillit.c"
 #include <assert.h>
 
 int	main(int argc, char **argv)
 {
 	if (argc == 2)
 	{
+		t_state *state = (t_state*)ft_memalloc(sizeof(t_state));
 		system(ft_strjoin("cd input_gen && python tetriminos.py ", argv[1]));
 		if (ft_atoi(argv[1]) >= 1 && ft_atoi(argv[1]) <= 26)
 		{
 			assert(validate_input("input_gen/ttmnfile") == 1);
+			assert(parse_input("input_gen/ttmnfile", state) == 1);
 			ft_putendl("input is valid, this is correct");
 		}
 		else
 		{
 			assert(validate_input("input_gen/ttmnfile") == 0);
+			assert(parse_input("input_gen/ttmnfile", state) == 0);
 			ft_putendl("input is invalid, this is correct");
 		}
 		return (0);
