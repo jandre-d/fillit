48a6c1fcc9297541d3556bdebfbe8ca9344d3b48
Thijs de Jong <tde-jong@student.codam.nl>
2019-01-31 18:21:03 +0100

    should fail now

--- test/input_check.c
+++ test/input_check.c
@@ -6,7 +6,7 @@
 /*   By: tde-jong <tde-jong@student.codam.nl>         +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2019/01/30 14:21:18 by tde-jong       #+#    #+#                */
-/*   Updated: 2019/01/31 18:18:43 by tde-jong      ########   odam.nl         */
+/*   Updated: 2019/01/31 18:20:57 by tde-jong      ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,14 +24,18 @@ int	main(int argc, char **argv)
 		system(ft_strjoin("cd input_gen && python tetriminos.py ", argv[1]));
 		if (ft_atoi(argv[1]) >= 1 && ft_atoi(argv[1]) <= 26)
 		{
-			assert(validate_input("input_gen/ttmnfile") == 1);
-			assert(parse_input("input_gen/ttmnfile", state) == 1);
+			if (validate_input("input_gen/ttmnfile") != 1)
+				return (1);
+			if (parse_input("input_gen/ttmnfile", state) != 1)
+				return (1);
 			ft_putendl("input is valid, this is correct");
 		}
 		else
 		{
-			assert(validate_input("input_gen/ttmnfile") == 0);
-			assert(parse_input("input_gen/ttmnfile", state) == 0);
+			if (validate_input("input_gen/ttmnfile") != 0)
+				return (1);
+			if (parse_input("input_gen/ttmnfile", state) != 0)
+				return (1);
 			ft_putendl("input is invalid, this is correct");
 		}
 		return (0);
