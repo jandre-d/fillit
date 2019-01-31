b622071c630d41579c2cbb30df44fb96f0b05639
jandre-d <jandre-d@student.codam.nl>
2019-01-31 15:21:48 +0100

    .
    .

--- map.c
+++ map.c
@@ -6,13 +6,20 @@
 /*   By: jandre-d <jandre-d@student.codam.nl>         +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2019/01/31 14:34:09 by jandre-d       #+#    #+#                */
-/*   Updated: 2019/01/31 14:45:21 by jandre-d      ########   odam.nl         */
+/*   Updated: 2019/01/31 15:21:44 by jandre-d      ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "map.h"
 
-t_byte	jr_sqrt(t_tetrimino list)
+static t_byte	set_initial_size_sqrt(int block_count)
+{
+	if (block_count == 0)
+		return (0);
+
+}
+
+static t_byte	set_initial_size_count(t_tetrimino list)
 {
 
 }
@@ -26,6 +33,7 @@ void	free_map(t_state state)
 {
 	t_byte i;
 
+	i = 0;
 	while (i < state.field_size)
 	{
 		ft_memdel((void **)state.field[i]);
@@ -37,6 +45,7 @@ int		alloc_map(t_state state)
 {
 	t_byte i;
 
+	i = 0;
 	if ((state.field = (char **)ft_memalloc(state.field_size)) == NULL)
 		return (-1);
 	while (i < state.field_size)
