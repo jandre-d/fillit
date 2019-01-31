4e57fbf3d969e8594de52f53e54c562efe68f258
jandre-d <jandre-d@student.codam.nl>
2019-01-31 14:09:16 +0100

    Update fillit.h
    .

--- fillit.h
+++ fillit.h
@@ -6,7 +6,7 @@
 /*   By: jandre-d <jandre-d@student.codam.nl>         +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2019/01/26 15:55:39 by tde-jong       #+#    #+#                */
-/*   Updated: 2019/01/31 14:01:30 by jandre-d      ########   odam.nl         */
+/*   Updated: 2019/01/31 14:09:08 by jandre-d      ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,8 +20,15 @@ typedef struct			s_tetrimino
 	char				**array;
 	char				w;
 	char				h;
-	char				end_char;
+	char				value;
 	struct s_tetrimino	*next;
-}						t_etrimino;
+}						t_tetrimino;
+
+typedef struct			s_state
+{
+	t_tetrimino			*tetriminos;
+	char				**field;
+	int					field_size;
+}						t_state;
 
 #endif
