311f47e8e46737d44dc11a811d480971bf32fd44
Thijs de Jong <tde-jong@student.codam.nl>
2019-01-31 18:38:44 +0100

    more test fix

--- test/main.c
+++ test/main.c
@@ -6,7 +6,7 @@
 /*   By: tde-jong <tde-jong@student.codam.nl>         +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2019/01/30 11:37:13 by tde-jong       #+#    #+#                */
-/*   Updated: 2019/01/30 14:32:59 by tde-jong      ########   odam.nl         */
+/*   Updated: 2019/01/31 18:38:32 by tde-jong      ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,14 +15,16 @@
 
 int	main(void)
 {
-	int i;
-	char *base;
+	int		i;
+	char	*base;
+	int		ret;
 
 	i = 0;
 	base = "./input_check ";
 	while (i < 28)
 	{
-		system(ft_strjoin(base, ft_itoa(i)));
+		if ((ret = system(ft_strjoin(base, ft_itoa(i)))))
+			abort();
 		i++;
 	}
 	return (0);
