05b9ac8e2b3bf353f2abb93bf0c838cc850e3293
Thijs de Jong <msnvanthijs@live.nl>
2019-02-01 15:17:24 +0100

    Input validator (#7)
    * Seems to be done
    
    * Update parse_input.c
    
    * Update parse_input.c
    
    * first attempt to add test, might break
    
    * Fix test
    
    * Don't parse input if it's invalid
    
    * fix test
    
    * should fail now
    
    * Try abort
    
    * revert back to assert
    
    * more test fix
    
    * Do not parse invalid file
    
    * extend tests
    
    * Add more tests
    
    * add <stdio.h> for printf
    
    * Run actual tests
    
    * more test fixing
    
    * fix
    
    * Update input_check.c
    
    * Update main.c
    
    * Update main.c
    
    * see what command is being run
    
    * add colour
    
    * fix
    
    * Update main.c
    
    * add stdio for printf
    
    * fix test command
    
    * Update main.c
    
    * Update main.c
    
    * 1 more testcase left to fix
    
    * Finally working

--- test/input_check.c
+++ test/input_check.c
@@ -6,30 +6,58 @@
 /*   By: tde-jong <tde-jong@student.codam.nl>         +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2019/01/30 14:21:18 by tde-jong       #+#    #+#                */
-/*   Updated: 2019/01/30 14:40:07 by tde-jong      ########   odam.nl         */
+/*   Updated: 2019/02/01 12:33:20 by tde-jong      ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../libft/libft.h"
 #include "../validate_input.c"
+#include "../parse_input.c"
+#include "../fillit.h"
 #include <assert.h>
+#include <stdio.h>
+
+
+#define KGRN "\x1B[32m"
+#define KNRM  "\x1B[0m"
 
 int	main(int argc, char **argv)
 {
-	if (argc == 2)
+	if (argc == 4 && ft_strcmp(argv[3], "input_gen") == 0)
 	{
+		t_state *state = (t_state*)ft_memalloc(sizeof(t_state));
 		system(ft_strjoin("cd input_gen && python tetriminos.py ", argv[1]));
 		if (ft_atoi(argv[1]) >= 1 && ft_atoi(argv[1]) <= 26)
 		{
-			assert(validate_input("input_gen/ttmnfile") == 1);
-			ft_putendl("input is valid, this is correct");
+			assert(validate_input(argv[2]) == 1);
+			printf("%svalidate_input returned 1%s\n", KGRN, KNRM);
+			assert(parse_input(argv[2], state) == 1);
+			printf("%sparse_input returned 1%s\n", KGRN, KNRM);
 		}
 		else
 		{
-			assert(validate_input("input_gen/ttmnfile") == 0);
-			ft_putendl("input is invalid, this is correct");
+			assert(validate_input(argv[2]) == 0);
+			printf("%svalidate_input returned 0%s\n", KGRN, KNRM);
 		}
 		return (0);
+	}
+	else if (argc == 2)
+	{
+		t_state *state2 = (t_state*)ft_memalloc(sizeof(t_state));
+		if ((ft_strstr(argv[1], "ko") == NULL) && (ft_strstr(argv[1], "bad") == NULL))
+		{
+			assert(validate_input(argv[1]) == 1);
+			printf("%svalidate_input returned 1%s\n", KGRN, KNRM);
+			assert(parse_input(argv[1], state2) == 1);
+			printf("%sparse_input returned 1%s\n", KGRN, KNRM);
+			return (0);
+		}
+		else
+		{
+			assert(validate_input(argv[1]) == 0 || parse_input(argv[1], state2) == 0);
+			printf("%svalidate_input returned 0%s\n", KGRN, KNRM);
+			return (0);
+		}
 	}
 	return (1);
 }
--- test/main.c
+++ test/main.c
@@ -6,24 +6,47 @@
 /*   By: tde-jong <tde-jong@student.codam.nl>         +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2019/01/30 11:37:13 by tde-jong       #+#    #+#                */
-/*   Updated: 2019/01/30 14:32:59 by tde-jong      ########   odam.nl         */
+/*   Updated: 2019/02/01 12:09:22 by tde-jong      ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../libft/libft.h"
 #include <unistd.h>
 
+#include <dirent.h>
+#include <stdio.h>
+
 int	main(void)
 {
-	int i;
-	char *base;
+	int		i;
+	char	*base;
+	int		ret;
 
 	i = 0;
 	base = "./input_check ";
 	while (i < 28)
 	{
-		system(ft_strjoin(base, ft_itoa(i)));
+		printf("\nrunning command: %s\n", ft_strjoin(base, ft_strjoin(ft_itoa(i), " input_gen/ttmnfile input_gen")));
+		if ((ret = system(ft_strjoin(base, ft_strjoin(ft_itoa(i), " input_gen/ttmnfile input_gen")))))
+			abort();
 		i++;
 	}
+
+	DIR *d;
+    struct dirent *dir;
+    d = opendir("input_example");
+    if (d)
+    {
+        while ((dir = readdir(d)) != NULL)
+        {
+			if (ft_strcmp(dir->d_name, "..") != 0 && ft_strcmp(dir->d_name, ".") != 0 && ft_strcmp(dir->d_name, ".DS_Store") != 0)
+			{
+				printf("\nrunning command: %s\n", ft_strjoin(base, ft_strjoin("input_example/", dir->d_name)));
+				if ((ret = system(ft_strjoin(base, ft_strjoin("input_example/", dir->d_name)))))
+					abort();
+			}
+		}
+        closedir(d);
+    }
 	return (0);
 }
