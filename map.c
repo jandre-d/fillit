202f495c3e24bfb3cfccaae25f12617e36fdb3cb
jandre-d <jandre-d@student.codam.nl>
2019-02-06 15:15:14 +0100

    finish
    finish

--- map.c
+++ map.c
@@ -6,7 +6,7 @@
 /*   By: jandre-d <jandre-d@student.codam.nl>         +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2019/01/31 14:34:09 by jandre-d       #+#    #+#                */
-/*   Updated: 2019/02/06 13:52:34 by jandre-d      ########   odam.nl         */
+/*   Updated: 2019/02/06 15:14:58 by jandre-d      ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -75,7 +75,7 @@ void			free_map(t_map *map)
 		ft_memdel((void **)&map->field[i]);
 		i++;
 	}
-	ft_memdel((void **)map->field);
+	ft_memdel((void **)&map->field);
 }
 
 int				alloc_map(t_map *map)
