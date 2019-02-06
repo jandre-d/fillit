134b38a6cc15f17bd11545f50d3ee679b95983a4
jandre-d <jandre-d@student.codam.nl>
2019-02-06 14:54:18 +0100

    .
    .

--- fillit.c
+++ fillit.c
@@ -6,7 +6,7 @@
 /*   By: jandre-d <jandre-d@student.codam.nl>         +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2019/01/26 15:55:14 by tde-jong       #+#    #+#                */
-/*   Updated: 2019/02/06 14:49:14 by jandre-d      ########   odam.nl         */
+/*   Updated: 2019/02/06 14:54:07 by jandre-d      ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,30 +21,26 @@ int	quit(char *msg)
 	return (1);
 }
 
-void	do_it(char *filename)
+int	main(int argc, char **argv)
 {
 	t_tetrimino *list;
 	t_map		*result;
 	int			fd;
 
-	fd = open(filename, O_RDONLY);
-	if (fd < 1)
-		return (quit("error"));
-	list = get_tetriminio_list(fd);
-	if (list == NULL)
-		return (quit("error"));
-	result = solve(list);
-	if (result == NULL)
-		return (quit("error"));
-	print_map(result);
-	free_alles(list, result);
-}
-
-int	main(int argc, char **argv)
-{
 	if (argc == 2)
 	{
-		do_it(argv[1]);
+		fd = open(argv[1], O_RDONLY);
+		if (fd < 1)
+			return (quit("error"));
+		list = get_tetriminio_list(fd);
+		if (list == NULL)
+			return (quit("error"));
+		close(fd);
+		result = solve(list);
+		if (result == NULL)
+			return (quit("error"));
+		print_map(result);
+		free_alles(list, result);
 	}
 	else
 	{
