7aaface7aac0be20f59eff7b4df3f92bfc544c12
Thijs de Jong <tde-jong@student.codam.nl>
2019-02-06 11:30:19 +0100

    Change 'usage:'

--- fillit.c
+++ fillit.c
@@ -6,7 +6,7 @@
 /*   By: jandre-d <jandre-d@student.codam.nl>         +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2019/01/26 15:55:14 by tde-jong       #+#    #+#                */
-/*   Updated: 2019/02/04 16:33:26 by tde-jong      ########   odam.nl         */
+/*   Updated: 2019/02/06 11:30:08 by tde-jong      ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -71,7 +71,7 @@ int	main(int argc, char **argv)
 	{
 		ft_putstr("usage: ");
 		ft_putstr(argv[0]);
-		ft_putstr(" source_file\n");
+		ft_putstr(" input_file\n");
 	}
 	return (0);
 }
